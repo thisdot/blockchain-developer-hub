a9810b1f395a947a0d92a05fe24297d7bff73cbb
Jerry Hogan <jerry.hogan@thisdot.co>
2023-02-21 11:16:07 +0100

    addressed comments

--- src/pages/api/user/tutorials/read.js
+++ src/pages/api/user/tutorials/read.js
@@ -85,7 +85,7 @@ export default async function (req, res) {
           if (savedData) {
             status = 200;
             resp = {
-              message: 'tracking successfull',
+              message: 'tracking successful',
             };
           } else {
             resp = {
