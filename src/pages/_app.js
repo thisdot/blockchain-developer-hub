0fb195cc79e575939d0b826c8dbed751adc16329
Jerry Hogan <jerry.hogan@thisdot.co>
2023-02-21 14:31:10 +0100

    auth added, mdx pages broken

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,16 +5,19 @@ import PropTypes from 'prop-types';
 import DefaultLayout from '@/layouts/DefaultLayout';
 import useGoogleTagManager from '@/hooks/useGoogleTagManager';
 import SolanaWalletProvider from 'src/context/SolanaWalletProvider';
+import { DataProvider } from 'src/context/DataProvider';
 
 function MyApp({ Component, pageProps }) {
   useGoogleTagManager(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING);
 
   return (
     <SolanaWalletProvider>
-      <DefaultLayout>
-        <DefaultSeo {...SEO} />
-        <Component {...pageProps} />
-      </DefaultLayout>
+      <DataProvider>
+        <DefaultLayout>
+          <DefaultSeo {...SEO} />
+          <Component {...pageProps} />
+        </DefaultLayout>
+      </DataProvider>
     </SolanaWalletProvider>
   );
 }
